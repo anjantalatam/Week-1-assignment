1d9c355420076505422ffe16e42c34b456d38d33
Anjan Talatam <tspa2117@gmail.com>
2023-06-13 21:41:28 +0530

    📦 NEW: solution: anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,10 @@
 */
 
 function isAnagram(str1, str2) {
-
+  return (
+    str1.toUpperCase().split("").sort().join("") ===
+    str2.toUpperCase().split("").sort().join("")
+  );
 }
 
 module.exports = isAnagram;
